b3d0e96ca6a4658fa4c5ad2c31a61feeceda0ce8
amman <amman@DESKTOP-VO0EAQH>
2025-04-30 13:11:38 +0100

    added validations to company and employees

--- ASPNetCoreDapper/Dto/CompanyForUpdateDto.cs
+++ ASPNetCoreDapper/Dto/CompanyForUpdateDto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPNetCoreDapper.Dto
 {
     public class CompanyForUpdateDto
     {
+        [Required(ErrorMessage = "Company name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(200, ErrorMessage = "Address cannot be longer than 200 characters")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(50, ErrorMessage = "Country cannot be longer than 50 characters")]
         public string Country { get; set; }
     }
 } 
--- ASPNetCoreDapper/Dto/EmployeeForUpdateDto.cs
+++ ASPNetCoreDapper/Dto/EmployeeForUpdateDto.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPNetCoreDapper.Dto
 {
     public class EmployeeForUpdateDto
     {
+        [Required(ErrorMessage = "Employee name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        [Range(18, 100, ErrorMessage = "Age must be between 18 and 100")]
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "Position is required")]
+        [StringLength(50, ErrorMessage = "Position cannot be longer than 50 characters")]
         public string Position { get; set; }
+
+        [Required(ErrorMessage = "Company ID is required")]
         public int CompanyId { get; set; }
     }
 } 
